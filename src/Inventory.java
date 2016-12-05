fbc281ccafd82053bb459898c256b1636807e3e3
r0bertcamp0s <r0bertcamp0s@yahoo.com>
2016-12-05 07:19:15 -0800

    First commit for inventory mgmt project

---
+++ src/Inventory.java
@@ -0,0 +1,44 @@
+/**
+ * Created by robertcampos on 12/2/16.
+ */
+public class Inventory {
+    private String name;
+    private int quantity;
+    private double price;
+
+    private Inventory() {
+
+    }
+
+    public Inventory(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
+
